10db77fb1e071c3cad7ab0ac492982f34daaaa8e
Regan <maknz@users.noreply.github.com>
2014-05-05 20:07:09 +1200

    Initial commit

---
+++ src/Maknz/Slack/SlackServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Maknz\Slack;
+
+use Illuminate\Support\ServiceProvider;
+
+class SlackServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('maknz/slack');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
